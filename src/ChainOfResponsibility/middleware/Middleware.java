488580ef3531af8678aeee0ef173673255a5982e
DESKTOP-AMPP2BT\Mking <msit26055@gmail.com>
2021-09-24 18:53:03 +0330

    create base MiddleWare

---
+++ src/ChainOfResponsibility/middleware/Middleware.java
@@ -0,0 +1,35 @@
+package ChainOfResponsibility.middleware;
+
+/**
+ * Base middleware class.
+ */
+public abstract class Middleware {
+
+    private Middleware next;
+
+    /**
+     * Builds chains of middleware objects.
+     */
+    public Middleware newtLink(Middleware next) {
+        this.next = next;
+        return next;
+    }
+
+    /**
+     * Subclasses will implement this method with concrete checks.
+     */
+    public abstract boolean check(String email, String password);
+
+    
+    /**
+     * Runs check on the next object in chain or ends traversing if we're in
+     * last object in chain.
+     */
+    public boolean checkNext(String email, String password) {
+        if (next == null)
+            return true;
+
+        return next.check(email, password);
+    }
+
+}
